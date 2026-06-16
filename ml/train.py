06b83a4b808203ea70bad661e551f40854777e29
mphele <mphelemoswane3@gmail.com>
2026-06-16 18:03:01 +0200

    model initialized

--- ml/train.py
+++ ml/train.py
@@ -75,9 +75,7 @@ if __name__ == "__main__":
     
     
     print(f"Training XGBoost Regressor...")
-    # TODO 2: Initialize the XGBoost model
-    # model = xgb.XGBRegressor(n_estimators=150, learning_rate=0.1, max_depth=6)
-    
+    model = xgb.XGBRegressor(n_estimators=150, learning_rate=0.1, max_depth=6)
     
     # TODO 3: Fit the model on your training data
     # model.fit(...)
